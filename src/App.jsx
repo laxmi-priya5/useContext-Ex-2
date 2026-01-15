ec7fd1de9227d261d238eb3428e08c5c3f1a74fb
Laxmipriya <laxmi.priyar2005@gmail.com>
2026-01-15 12:33:41 +0530

    add:Provide a Value
    The Provider is like a “power socket” that supplies data to everything inside it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
+import { CurrentUserContext } from "./currentUserContext";
 
 function App() {
   
 
   return (
     <>
-      
+    <CurrentUserContext.Provider value={{ name: "Laxmipriya" }}>
+      <Profile />
+    </CurrentUserContext.Provider>
+   
         
     </>
   )
 }
+function Profile() {
+  return <p>Profile goes here</p>;
+}
+
 
 export default App
